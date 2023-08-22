f28ea4a39722e2db6b96cba5f88ce8021f2fd6e6
Raja <rajawaheed5473238@gmail.com>
2023-08-22 15:39:38 +0500

    obj3dt

---
+++ studwnt.js
@@ -0,0 +1,7 @@
+let teacher = {
+    name: "Zeeshan",
+    exp: 10
+};
+teacher.name = "akhter";
+console.log(teacher.name);
+export {};
---
+++ studwnt.ts
@@ -0,0 +1,7 @@
+let teacher : {name: string, exp: number} = {
+  name: "Zeeshan",
+  exp: 10
+}
+ teacher.name="akhter"
+ console.log(teacher.name);
+ 
