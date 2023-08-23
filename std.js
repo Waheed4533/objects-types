081ed3cbde553b80b23d56606e7958766e232d6a
Raja <rajawaheed5473238@gmail.com>
2023-08-23 19:42:48 +0500

    objects

---
+++ std.js
@@ -0,0 +1,27 @@
+//// anonymous
+let teacher = {
+    name: "Zeeshan",
+    exp: 10
+};
+let person = {
+    name: "Zeeshan",
+    exp: 10,
+    age: 56
+};
+teacher = person;
+console.log(teacher);
+let student = {
+    name: "Hira",
+    age: 30
+};
+let std2 = {
+    name: "Asif",
+    age: 10
+};
+std2.name = "Khalid";
+std2.age = 50;
+let Dog = {
+    sound: "Bark",
+    gender: 1
+};
+export {};
--- studwnt.js
+++ studwnt.js
@@ -1,7 +1,49 @@
+//What is an Object?
+//Object is something which possess 
+// 1. Properties
+// 2. Functions
+// Notation ->   {} Curly Bracktes
 let teacher = {
     name: "Zeeshan",
-    exp: 10
+    experience: "10",
+    teach() {
+        console.log("The Responsiblity of a Good Teacher is to Deliver the knowledge not teaching!");
+    },
+    check(marks, name) {
+        console.log(`Your Name ${name} & Marks are : ${marks}`);
+    }
 };
-teacher.name = "akhter";
-console.log(teacher.name);
+//To Access/Reterive the values of an object
+let objName = teacher.name; //Storing Object property in a variable
+let objExpr = teacher.experience; //Storing  Object property in a variable
+//console.log(`Name:${objName}, Experience: ${objExpr}`);  //Printing the value of object property through variable
+//Directly accessing the values of properties of object teacher
+//console.log(`Name:${teacher.name}, Experience: ${teacher.experience}`);  
+//To Set/Change the value of object's property
+teacher.name = "Zia Ullah Khan";
+teacher.experience = "50";
+//console.log(`Name:${teacher.name}, Experience: ${teacher.experience}`);              
+objName = "Sadiq";
+objExpr = "39";
+//console.log(`Name:${teacher.name}, Experience: ${teacher.experience}`);              
+//Inline or Annonimous Objects
+printObject({
+    key: "01",
+    value: "Alpha"
+});
+let annObj;
+function printObject(obj) {
+    console.log(obj);
+    let hasKeyProp = obj.hasOwnProperty("key");
+    let keys = Object.keys;
+    annObj = obj;
+}
+console.log(annObj);
 export {};
+// printObject({
+//     objName,
+//     objExpr
+// });
+//Objects Methods/Function Invoking
+// teacher.teach();
+// teacher.check(10,"Sadiq");
